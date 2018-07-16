048976e69771a26c4a6bbee7ed5651354b49cdc7
Philip Diaz <lambdahands@gmail.com>
2018-07-16 16:48:14 -0500

    zprint styles

--- src/batcher/core.clj
+++ src/batcher/core.clj
@@ -11,11 +11,15 @@
     (resolve-fn ctx args values)))
 
 (defn build-batcher-opts
-  [{:keys [interval capacity] :or {interval 1 capacity 500}}]
-  (apply concat {:interval interval :capacity capacity}))
+  [{:keys [interval capacity]
+    :or   {interval 1
+           capacity 500}}]
+  [:interval interval :capacity capacity])
 
 (defn init-batcher
-  [{:keys [batches] :as ctx} batch args resolve-fn opts]
+  [{:keys [batches]
+    :as   ctx}
+   batch args resolve-fn opts]
   (let [batcher-opts (build-batcher-opts opts)]
     (or (get @batches batch)
         (let [resolver (build-resolver ctx batch args resolve-fn opts)
@@ -26,13 +30,19 @@
 (defn on-batch-error
   [result batch]
   (fn [error]
-    (let [info {:batch batch :message (str "Exception: " (.getMessage error))}]
-      (resolve/deliver! result nil info))))
+    (resolve/deliver! result
+                      nil
+                      {:batch   batch
+                       :message (str "Exception: " (.getMessage error))})))
 
 (defn with-batching
-  [resolve-fn & {:keys [interval max-capacity] :as opts}]
-  (fn [{:keys [batches ::graphql/selection] :as ctx} args value]
-    (if-let [batch (:batch (:field-definition selection))]
+  [resolve-fn &
+   {:keys [interval max-capacity]
+    :as   opts}]
+  (fn [{:keys [batches ::graphql/selection]
+        :as   ctx}
+       args value]
+    (if-let [batch (and batches (:batch (:field-definition selection)))]
       (let [batcher (init-batcher ctx batch args resolve-fn opts)
             result (resolve/resolve-promise)]
         (grouper/submit! batcher
