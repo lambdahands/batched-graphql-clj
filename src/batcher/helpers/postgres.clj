25242973f0d27c1d4d84c7303bedb5b595963eb2
Philip Diaz <lambdahands@gmail.com>
2018-07-25 15:49:46 -0500

    Add collect-results1 function

--- src/batcher/helpers/postgres.clj
+++ src/batcher/helpers/postgres.clj
@@ -3,9 +3,9 @@
 (defn build-values-tuples
   [kws values]
   (->> values
-       (map (apply juxt kws))
-       (map-indexed #(conj %2 %1))
-       (into [])))
+    (map (apply juxt kws))
+    (map-indexed #(conj %2 %1))
+    (into [])))
 
 (defn build-values-table
   [kws values]
@@ -19,5 +19,11 @@
 (defn collect-results
   [values results]
   (let [step (fn [acc v] (update acc (:_index v) conj v))
+        init (vec (repeat (count values) nil))]
+    (reduce step init results)))
+
+(defn collect-results1
+  [values results]
+  (let [step (fn [acc v] (assoc acc (:_index v) v))
         init (vec (repeat (count values) nil))]
     (reduce step init results)))
